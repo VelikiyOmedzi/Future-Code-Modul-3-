3c9fe5505ec501f176849ff1980af3c1bef0d00f
VelikiyOmedzi <126394658+VelikiyOmedzi@users.noreply.github.com>
2023-05-21 17:36:10 +0300

    Add files via upload

--- HomeWork2/src/Main.java
+++ HomeWork2/src/Main.java
@@ -1,6 +1,32 @@
 public class Main {
     public static void main(String[] args) {
+        OmedziList<Integer> omedzi = new OmedziList<>();
+        omedzi.add(18);
+        omedzi.add(78);
+        omedzi.add(17);
+        omedzi.add(651);
+        omedzi.add(43);
+        omedzi.add(12);
+        omedzi.add(0);
+        omedzi.add(1);
+        omedzi.add(3);
+        omedzi.add(66);
 
+        System.out.println(omedzi.get(3));
+
+        System.out.println("Then: ");
+
+        for (int i = 0; i < omedzi.getSize(); i++) {
+            System.out.print(omedzi.get(i) + "  ");
+        }
+
+        omedzi.remove(3);
+
+        System.out.println("\nAfter: ");
+
+        for (int i = 0; i < omedzi.getSize(); i++) {
+            System.out.print(omedzi.get(i) + "  ");
+        }
     }
 
 
